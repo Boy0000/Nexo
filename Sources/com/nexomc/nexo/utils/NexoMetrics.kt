10bd7b6fde9cbbc6d7b1b0d5165094cd90e629b9
MediumCraft <mediumcraft@users.noreply.github.com>
2025-01-18 01:22:22 +0000

    Update code to latest snapshot

--- Sources/com/nexomc/nexo/utils/NexoMetrics.kt
+++ Sources/com/nexomc/nexo/utils/NexoMetrics.kt
@@ -1,7 +1,15 @@
 package com.nexomc.nexo.utils
 
+import com.nexomc.nexo.NexoPlugin
+import org.bstats.bukkit.Metrics
+import org.bstats.charts.SimplePie
+
 object NexoMetrics {
     fun initializeMetrics() {
-
+        val metrics = Metrics(NexoPlugin.instance(), 23930)
+        metrics.addCustomChart(SimplePie("marketplace") {
+            if (VersionUtil.isCompiled) return@SimplePie "Compiled"
+            return@SimplePie "Unknown"
+        })
     }
 }
