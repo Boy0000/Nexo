9b0a1e006a3318c36fff4734a7ec2d440b45a97e
MediumCraft <mediumcraft@users.noreply.github.com>
2025-02-20 01:27:21 +0000

    Update code to latest snapshot

--- Sources/com/nexomc/nexo/utils/NexoDatapack.kt
+++ Sources/com/nexomc/nexo/utils/NexoDatapack.kt
@@ -9,15 +9,15 @@ import org.bukkit.Bukkit
 open class NexoDatapack(key: String, description: String) {
 
     val defaultWorld = Bukkit.getWorlds().first()
-    val isFirstInstall: Boolean get() = Bukkit.getDatapackManager().packs.map { Key.key(it.name) }.none(datapackKey::equals)
-    val datapackEnabled: Boolean get() = Bukkit.getDatapackManager().getPack(datapackKey.value())?.isEnabled ?: false
+    val isFirstInstall: Boolean get() = Bukkit.getDatapackManager().packs.map { it.name }.none(datapackName::equals)
+    val datapackEnabled: Boolean get() = Bukkit.getDatapackManager().getPack(datapackName)?.isEnabled ?: false
     private val datapackMeta = JsonObject().apply {
         add("pack", JsonObject().apply {
             addProperty("pack_format", NMSHandlers.handler().datapackFormat())
             addProperty("description", description)
         })
     }
-    val datapackKey = Key.key("minecraft:file/$key")
+    val datapackName = "file/$key"
     val datapackFile = defaultWorld.worldFolder.resolve("datapacks/$key")
 
     init {
@@ -33,9 +33,9 @@ open class NexoDatapack(key: String, description: String) {
     internal fun enableDatapack(enabled: Boolean) {
         SchedulerUtils.callSyncMethod {
             if (!VersionUtil.isPaperServer) return@callSyncMethod
-            if (VersionUtil.below("1.21.1")) return@callSyncMethod Logs.logWarn("Could not enable ${datapackKey.value()} datapack, use /datapack-command")
+            if (VersionUtil.below("1.21.1")) return@callSyncMethod Logs.logWarn("Could not enable $datapackName datapack, use /datapack-command")
             //Bukkit.getDatapackManager().refreshPacks()
-            Bukkit.getDatapackManager().getPack(datapackKey.value())?.takeUnless { it.isEnabled == enabled }?.isEnabled = enabled
+            Bukkit.getDatapackManager().getPack(datapackName)?.takeUnless { it.isEnabled == enabled }?.isEnabled = enabled
         }
     }
 }
